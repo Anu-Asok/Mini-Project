c60886e6a5efaaad1d68534c7c8405cb037b2a6f
JacobRoy <jacobroy8597@gmail.com>
2017-11-09 21:40:43 +0530

    Added submit station

--- admin/station.php
+++ admin/station.php
@@ -51,20 +51,14 @@
       </a>
     </div>
     <div class="ui container">
-      <form class="ui form">
+      <form class="ui form" action="/miniproject/admin/station_submit.php" method="post">
         <div class="field">
-          <label>First Name</label>
-          <input type="text" name="first-name" placeholder="First Name">
+          <label>Station Name</label>
+          <input type="text" name="s_name" placeholder="Station Name">
         </div>
         <div class="field">
-          <label>Last Name</label>
-          <input type="text" name="last-name" placeholder="Last Name">
-        </div>
-        <div class="field">
-          <div class="ui checkbox">
-            <input type="checkbox" tabindex="0" class="hidden">
-            <label>I agree to the Terms and Conditions</label>
-          </div>
+          <label>Station Code</label>
+          <input type="text" name="s_code" placeholder="Station Code">
         </div>
         <button class="ui button" type="submit">Submit</button>
       </form>
---
+++ admin/station_submit.php
@@ -0,0 +1,25 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 'On');
+  $servername = "localhost";
+  $username = "root";
+  $password = "password";
+  $dbname = "miniproject";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sname=$_POST['s_name'];
+  $scode=$_POST['s_code'];
+  $sql = "INSERT INTO `Station` (`Station_Code`, `Station_Name`)
+          VALUES ('$scode', '$sname')";
+
+  if ($conn->query($sql) === TRUE) {
+      echo "<script>alert('Station added successfully');window.location.href='/miniproject/admin/station.php';</script>";
+  } else {
+      echo "<script>alert('Station already added!');window.location.href='/miniproject/admin/station.php';</script>";
+  }
+
+  $conn->close();
+?>
