35d011a8799339e7ca5f0cf7a8a5e60a375e2541
Anupam Asok <anu.asok701@gmail.com>
2017-11-18 19:58:47 +0530

    Authentication works

--- index.php
+++ index.php
@@ -1,87 +1,98 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="/miniproject/css/login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.js"></script>
-    <link rel="shortcut icon" href="/miniproject/logo.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui-calendar/0.0.8/calendar.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui-calendar/0.0.8/calendar.min.css" />
-  </head>
-  <body>
-    <?php
-      session_start();
-      if(isset($_SESSION["email"])){
-          echo "<script>window.location.href='/miniproject/home.php';</script>";
-      }
-    ?>
-    <div class="ui container">
-      <div class="ui secondary menu">
-        <div id="auth-menu">
-          <a class="item active" data-tab="login" style="display:inline-block;">Login</a>
-          <a class="item" data-tab="signup" style="display:inline-block;">Sign up</a>
-        </div>
-      </div>
-      <div id="tab">
-        <div class="ui active tab segment" data-tab="login">
-          <form action="login.php" method="post">
-            <div id="login-form">
-              <div class="ui fluid left icon input">
-                <input type="email" name="email" placeholder="Email" >
-                <i class="user icon"></i>
-              </div>
-              <br>
-              <div class="ui fluid left icon input">
-                <input type="password" name="password" placeholder="Password" >
-                <i class="sign in icon"></i>
-              </div>
-              <div class="ui divider"></div>
-              <input type="submit" class="ui teal fluid button" value="Login">
-            </div>
-          </form>
-        </div>
-        <div class="ui tab segment" data-tab="signup">
-          <form action="signup.php" method="post">
-            <div id="signup-form">
-              <div class="ui fluid left icon input">
-                <input type="text" placeholder="Name" name="name" required>
-                <i class="font icon"></i>
-              </div>
-              <br>
-              <div class="ui fluid left icon input">
-                <input type="email" placeholder="Email" name="email" required>
-                <i class="user icon"></i>
-              </div>
-              <br>
-              <div class="ui fluid left icon input">
-                <input type="password" placeholder="Password" name="password" minlength="8" required>
-                <i class="sign in icon"></i>
-              </div>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Sign-Up/Login Form</title>
+  <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
-              <br>
-              <div class="ui fluid left icon input">
-                <input type="text" placeholder="Phone Number" name="phonenumber" minlength="10" maxlength="10" required>
-                <i class="text telephone icon"></i>
-              </div>
-              <br>
-              <select class="ui fluid normal dropdown" name="gender" required>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
-              <div class="ui divider"></div>
-              <input type="submit" class="ui teal fluid button" value="Sign Up">
+
+      <link rel="stylesheet" href="/miniproject/css/style.css">
+
+
+</head>
+
+<body>
+  <div class="form">
+
+      <ul class="tab-group">
+        <li class="tab active"><a href="#signup">Sign Up</a></li>
+        <li class="tab"><a href="#login">Log In</a></li>
+      </ul>
+
+      <div class="tab-content">
+        <div id="signup">
+          <form action="signup.php" method="post">
+
+            <div class="field-wrap">
+              <label>
+                Name<span class="req">*</span>
+              </label>
+              <input type="text" name="name" required autocomplete="off" />
             </div>
+
+          <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email" name="email" required autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Set A Password<span class="req">*</span>
+            </label>
+            <input type="password" name="password" required autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Phone<span class="req">*</span>
+            </label>
+            <input type="text" name="phonenumber" required autocomplete="off"/>
+          </div>
+          <select name="gender" style="width:100%; height:40px;">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          <br>
+          <br>
+          <button type="submit" class="button button-block"/>Signup</button>
+
           </form>
+
         </div>
-      </div>
-    </div>
-    <script>
-      $('.menu .item').tab();
-      $('.dropdown').dropdown();
-    </script>
-  </body>
+
+        <div id="login">
+
+          <form action="login.php" method="post">
+
+            <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email" name="email" required autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password" name="password" required autocomplete="off"/>
+          </div>
+
+          <button class="button button-block"/>Log In</button>
+
+          </form>
+
+        </div>
+
+      </div><!-- tab-content -->
+
+</div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script  src="/miniproject/script/index.js"></script>
+
+</body>
 </html>
